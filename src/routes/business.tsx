a8c9d13bd045c528595f626636748760b20b8fb1
Laydo74 <seotivity@gmail.com>
2026-09-23 22:48:59 +0100

    Refine business wellbeing page metadata

--- src/routes/business.tsx
+++ src/routes/business.tsx
@@ -4,5 +4,5 @@ import { EditorialPage } from "@/components/site/PublicPage";
 import { pageHead } from "@/components/site/PageMeta";
 import { pageContent } from "@/lib/site-content";
 const content=pageContent.business;
-export const Route=createFileRoute("/business")({head:()=>pageHead("Executive Wellbeing & Private Advisory | GULFWELLBEING","Private executive wellbeing advisory for founders, executives and senior professionals navigating pressure, burnout prevention, leadership and responsibility."),component:Page});
+export const Route=createFileRoute("/business")({head:()=>pageHead("Business Wellbeing & Private Executive Advisory | GULFWELLBEING","Private business wellbeing advisory for founders, executives, senior professionals and organisations navigating pressure, leadership, burnout prevention and the human side of performance."),component:Page});
 function Page(){return <SiteShell><EditorialPage page="business" {...content}/></SiteShell>}
